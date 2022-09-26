21810c2ac52d1d142a820bf391832cecca6c9632
大大狐狸cyberfox_meow <bigbigfox@office.qianbai.tw>
2022-09-26 18:59:47 -0500

    20220926

--- utils.h
+++ utils.h
@@ -6,6 +6,17 @@
 #define UNTITLED_UTILS_H
 
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define VMRSS_LINE 17
+#define VMSIZE_LINE 13
+#define PROCESS_ITEM 14
+
 class utils {
 
 };
